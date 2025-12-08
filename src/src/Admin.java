a925fea586c24afddbe630a99a133f8ee1f0b6f7
spiecee <spiecee@rose-hulman.edu>
2025-12-08 13:33:30 -0500

    Admin and ClubMember work

--- src/src/Admin.java
+++ src/src/Admin.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Admin extends Person {
     public Admin(String emailAddress) {
         super(emailAddress);
+    }
+
+    public void showDataForAllClubMembers(ArrayList<Person> listOfPeople){
+        for (Person p : listOfPeople) {
+            if (p instanceof ClubMember clubMember) {
+                System.out.println(clubMember.toString());
+            }
+        }
     }
 }
--- src/src/ClubMember.java
+++ src/src/ClubMember.java
@@ -1,6 +1,26 @@
-public class ClubMember extends Person {
+import java.util.ArrayList;
 
-    public ClubMember(String emailAddress) {
+public class ClubMember extends Person {
+    private String id;
+
+    public ClubMember(String emailAddress, String id) {
         super(emailAddress);
+        this.id = id;
+    }
+
+    public String toString(){
+        return "";
+    }
+
+    public void generateSongReport(ArrayList<String> requests){
+        for (String request : requests) {
+            if (request.equals(this.id)) {
+                System.out.println(request);
+            }
+        }
+    }
+
+    public void reportSongDelivered(String nameOfSong, String sweetheartName){
+
     }
 }
