06e331175150481679738f5967e11518de2d4e0b
spiecee <spiecee@rose-hulman.edu>
2025-12-08 13:21:07 -0500

    main class setup

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class Main {
+    private ArrayList<Person> listOfPeople = new ArrayList<Person>();
+    private ArrayList<Song> listOfSongs = new ArrayList<Song>();
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
