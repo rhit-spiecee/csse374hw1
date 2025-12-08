0c1ee2e59b0c85785ffb976258b687f99d3b4f82
SeokHyun Chang <89899621+rhit-changs@users.noreply.github.com>
2025-12-08 14:01:59 -0500

    main update 1

--- src/src/Main.java
+++ src/src/Main.java
@@ -19,24 +19,57 @@ public class Main {
 
             String userInput = scanner.nextLine();
 
-            if (userInput == "1") {
+            if (userInput.equals("1")) {
                 System.out.println("You selected: Customer - Order a song for Valentine’s Day");
+                System.out.println("Order a song for Valentine’s Day...\n" +
+                        "Select the song from this list:\n" +
+                        "1 - \"Can't Help Falling in Love\" by Elvis Presley\n" +
+                        "2 - \"At Last\" by Etta James\n" +
+                        "3 - \"Unchained Melody\" by The Righteous Brothers\n" +
+                        "4 - \"Perfect\" by Ed Sheeran\n" +
+                        "5 - \"All of Me\" by John Legend\n" +
+                        "6 - \"A Thousand Years\" by Christina Perri\n" +
+                        "7 – \"Make You Feel My Love\" by Adele\n" +
+                        "Your choice (1 – 7):\n");
+                userInput = scanner.nextLine();
+                switch (userInput) {
+                    case "1":
+                        System.out.println("You selected: 1 - \"Can't Help Falling in Love\" by Elvis Presley");
+                        break;
+                    case "2":
 
-            }
-            if (userInput == "2") {
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+
+                        break;
+                    case "7":
+
+                        break;
+                    default:
+                        System.out.println("Enter valid options though 1-7");
+                }
+
+            } else if (userInput.equals("2")) {
                 System.out.println("You selected: Club members – Get a report of requests for your song");
 
-            }
-            if (userInput == "3") {
+            } else if (userInput.equals("3")) {
                 System.out.println("You selected: Club members – Report back that your songs are done");
 
-            }
-            if (userInput == "4") {
+            } else if (userInput.equals("4")) {
                 System.out.println("You selected: Admin - Show data for all club members");
 
+            } else {
+                System.out.println("Enter valid options though 1-4");
             }
-            System.out.println("Enter valid options though 1-4");
-
 
         }
     }
