8483648b3a632d6cdf3b60a3ed4e953ea1b4fce1
spiecee <spiecee@rose-hulman.edu>
2025-12-08 13:16:30 -0500

    song class setup

--- src/src/Song.java
+++ src/src/Song.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Song {
+    private String name;
+    private String artist;
+    private ArrayList<String> requests;
+    private int memberId;
+
+    public Song(String name, String artist, int memberId) {
+        this.name = name;
+        this.artist = artist;
+        this.memberId = memberId;
+        requests = new ArrayList<>();
+    }
+
+    public void addRequest(String sweetheartName) {
+        requests.add(sweetheartName);
+    }
 }
